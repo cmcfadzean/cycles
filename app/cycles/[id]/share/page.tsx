05dce65a273a43ad93da613eaeba44565e7e1d74
Court McFadzean <cmcfadzean1@gmail.com>
2026-01-05 15:57:10 -0800

    Change logo to layers/stack icon

--- app/cycles/[id]/share/page.tsx
+++ app/cycles/[id]/share/page.tsx
@@ -315,7 +315,7 @@ export default function ShareCyclePage() {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                  d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
                 />
               </svg>
             </div>
